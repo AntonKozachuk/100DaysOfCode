b804b03dea29748446f0101f461aa43a46fcff3f
user <invulnarble@gmail.com>
2020-02-21 07:54:20 +0200

    finished

--- Book List Project/app.js
+++ Book List Project/app.js
@@ -15,17 +15,41 @@ UI.prototype.addBookToList = function(book) {
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td><a href='#" class='delete'>X</a></td>
+        <td><a href="#" class="delete">X<a></td>
     `;
 
     list.appendChild(row);
 }
 
+UI.prototype.showAlert = function(message, className) {
+    const div = document.createElement('div');
+
+    div.className = `alert ${className}`;
+
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector('.container');
+
+    const form = document.querySelector('#book-form');
+
+    container.insertBefore(div, form);
+
+    setTimeout(function() {
+        document.querySelector('.alert').remove();
+    }, 3000);
+};
+
+UI.prototype.deleteBook = function(target) {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove();
+    }
+};
+
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
     document.getElementById('isbn').value = '';
-}
+};
 
 
 
@@ -38,11 +62,25 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
 
     const ui = new UI();
 
-    console.log(ui);
+    if(title === '' || author === '' || isbn === '') {
+        ui.showAlert('Please fill an all fields', 'error');
+    } else {
+        ui.addBookToList(book);
 
-    ui.addBookToList(book);
+        ui.showAlert('Book Added', 'success');
 
-    ui.clearFields();
-    
+        ui.clearFields();
+    }
+  
+        e.preventDefault();
+});
+
+document.getElementById('book-list').addEventListener('click', function(e) {
+    const ui = new UI();
+
+    ui.deleteBook(e.target);
+
+    ui.showAlert('Book Removed!', 'success');
+
     e.preventDefault();
 });
